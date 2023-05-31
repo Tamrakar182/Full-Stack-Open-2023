3e65b93e81c6094fb2ca3e0a81b06c14d2abf196
Manjul <manjultamrakar4@gmail.com>
2023-05-31 17:13:47 +0545

    1.7 end

--- part1/unicafe/unicafe/src/App.js
+++ part1/unicafe/unicafe/src/App.js
@@ -9,17 +9,68 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positivePercent, setPositivePercent] = useState(0)
+
+  const handleGood = () => {
+    setGood(good+1)
+
+    const updatedGood = good+1
+    setTotal(updatedGood+neutral+bad)
+
+    const updatedTotal = total+1
+    setAverage((updatedGood-bad)/updatedTotal)
+
+    setPositivePercent((updatedGood/updatedTotal)*100)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+
+    const updatedNeutral = neutral+1
+    setTotal(good+bad+updatedNeutral)
+
+    const updatedTotal = total+1
+    setPositivePercent((good/updatedTotal)*100)
+  }
+
+  const handleBad = () => {
+    setBad(bad+1)
+
+    const updatedBad = bad+1
+    setTotal(good+neutral+updatedBad)
+
+    const updatedTotal = total+1
+    setAverage((good-updatedBad)/updatedTotal)
+
+    setPositivePercent((good/updatedTotal)*100)
+  }
+
 
   return (
     <div>
       <Heading headingText="give feedback" />
-      <Button text="good" clickAction={() => setGood(good+1)} />
-      <Button text="neutral" clickAction={() => setNeutral(neutral+1)} />
-      <Button text="bad" clickAction={() => setBad(bad+1)} />
+      <Button 
+        text="good" 
+        clickAction = {handleGood}
+      />
+      <Button 
+        text="neutral" 
+        clickAction = {handleNeutral} 
+      />
+      <Button 
+        text="bad" 
+        clickAction = {handleBad} 
+      />
+      
       <Heading headingText="statistics" />
       good {good} <br/>
       neutral {neutral} <br/>
       bad {bad} <br/>
+      all {total} <br/>
+      average {average} <br/>
+      positive {positivePercent}% <br/>
     </div>
   )
 }
