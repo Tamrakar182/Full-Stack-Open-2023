d799df1aee0323de23cccb39dd03f0551b77d775
Manjul <manjultamrakar4@gmail.com>
2023-06-25 22:23:31 +0545

    4.1 satrt

--- part2/countries/src/services/country.js
+++ part2/countries/src/services/country.js
@@ -17,47 +17,4 @@ const getWeather = (capital) => {
     return req.then(res => res.data)
 }
 
-
-// Object Format is like this
-
-// {
-//     "name": {},
-//     "tld": [],
-//     "cca2": "KW",
-//     "ccn3": "414",
-//     "cca3": "KWT",
-//     "cioc": "KUW",
-//     "independent": true,
-//     "status": "officially-assigned",
-//     "unMember": true,
-//     "currencies": {},
-//     "idd": {},
-//     "capital": [],
-//     "altSpellings": [],
-//     "region": "Asia",
-//     "subregion": "Western Asia",
-//     "languages": {},
-//     "translations": {},
-//     "latlng": [],
-//     "landlocked": false,
-//     "borders": [],
-//     "area": 17818,
-//     "demonyms": {},
-//     "flag": "🇰🇼",
-//     "maps": {},
-//     "population": 4270563,
-//     "fifa": "KUW",
-//     "car": {},
-//     "timezones": [
-//     "UTC+03:00"
-//     ],
-//     "continents": [],
-//     "flags": {},
-//     "coatOfArms": {},
-//     "startOfWeek": "sunday",
-//     "capitalInfo": {},
-//     "postalCode": {}
-//    }
-
-
 export default {getCountry, getAll, getWeather}
